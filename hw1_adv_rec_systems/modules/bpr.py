1. updated config file with new parameters 2. Added regularization 3. added top n precision 4. fixed an issue with HS module 5. continue to improve the data prep model

hw1_adv_rec_systems/modules/bpr.py
@@ -11,10 +11,13 @@ import matplotlib.pyplot as plt
 import random
 
 class BPR:
-    def __init__(self, k=15, lr_u=0.01, lr_i=0.01,
-                 lr_j=0.01, n_users=0, n_items=0,
+    def __init__(self,
+                 k=15,
+                 lr_u=0.01, lr_i=0.01,lr_j=0.01,
+                 regularizers=dict(au=1e-1,av=1e-1),
+                 n_users=0, n_items=0,
                  sample_method='Uniform',
-                 max_epochs=100,early_stop_threshold=0.001,early_stopping_lag=2):
+                 max_epochs=10,early_stop_threshold=0.001,early_stopping_lag=10):
         self.k = k  # dimension to represent user/item vectors
         self.lr_u = lr_u
         self.lr_i = lr_i
@@ -28,12 +31,13 @@ class BPR:
         self.sample_method = sample_method
         self.early_stop_threshold=early_stop_threshold
         self.early_stopping_lag = early_stopping_lag
+        self.regularizers=regularizers
 
     def step(self, u, i, j, e_u_i_j):
         self.users[u] += self.lr_u * (
-                    e_u_i_j * (self.items[i] - self.items[j]))
-        self.items[i] += self.lr_i * (e_u_i_j * self.users[u])
-        self.items[j] += -self.lr_j * (e_u_i_j * self.users[u])
+                    e_u_i_j * (self.items[i] - self.items[j])) -self.lr_u*self.regularizers['au']*self.users[u]
+        self.items[i] += self.lr_i * (e_u_i_j * self.users[u]) -self.lr_i*self.regularizers['av']*self.items[i]
+        self.items[j] += -self.lr_j * (e_u_i_j * self.users[u]) -self.lr_j*self.regularizers['av']*self.items[j]
 
     def predict(self, u, i, j):
         pred = self.users[u].dot(self.items[i].T) - self.users[u].dot(
@@ -78,7 +82,7 @@ class BPR:
         the score for each goes as follows
         the number of times the prediction chance for the positive is higher than rest of negatives:
             sigmoid(xuT*vi) > sigmoid(XuTvj) for every j element in val set
-            note, we dont sigmoid since when x1>x2 then sigmoid(x1)>sigmoid(x2)
+            note, we dont apply sigmoid since when x1>x2 then sigmoid(x1)>sigmoid(x2)
         """
         total_auc=0
         for u,pos,neg in val_list:
@@ -105,6 +109,25 @@ class BPR:
             total_loss+=np.log(self.sigmoid(np.dot(vis,self.users[[u],:].T)-np.dot(vjs,self.users[[u],:].T))).sum()
         return total_loss/count_items
 
+    def precision_at_n(self,n,train_list):
+        precision=0
+        for u,pos,neg in train_list:
+            pred_u=self.users[[u]].dot(self.items.T).flatten()
+            topn=np.argsort(pred_u)[-n:][::-1]
+            precision+=len(set(topn).intersection(set(pos))) / n
+        return precision/len(train_list)
+
+    def classification_accuracy(self,val_list):
+        accuracy = 0
+        for u, pos, neg in val_list:
+            vis = self.items[pos, :]
+            if len(neg) < len(pos):
+                neg = self._extend_neg(neg, len(pos))
+            end_j = len(pos)
+            vjs = self.items[neg[:end_j], :]
+            correct=np.dot(vis, self.users[[u], :].T) > np.dot(vjs, self.users[[u], :].T)
+            accuracy+=correct.flatten().sum()/len(pos)
+        return accuracy/len(val_list)
 
     def save_params(self, path_out_u, path_out_i):
         with open(path_out_u, 'wb') as f:
@@ -122,19 +145,25 @@ class BPR:
         best_auc_valid = 0
         last_epoch_auc_valid = 0
         last_epoch_decrease = False
-        self.loss_curve = dict(training_loglike=[], validation_loglike=[], validation_auc=[])
+        self.loss_curve = dict(training_loglike=[],
+                               validation_loglike=[],
+                               validation_auc=[],
+                               val_accuracy=[],
+                               precision_at_5=[])
         while True and self.current_epoch<=self.max_epochs:
             print('epoch:', self.current_epoch)
             # ----  suffling the users ---- #
             random.shuffle(train_list)
             train_likelihood  = self.run_epoch(mspu=4000,train_list=train_list)
             # ----  updating losses and scores ---- #
+            #TODO: consider create a prediction matrix and have all losses use those scores instead of having each one calculating it
             self.loss_curve['training_loglike'].append(self.loss_log_likelihood(train_list))
             self.loss_curve['validation_loglike'].append(self.loss_log_likelihood(val_list))
             self.loss_curve['validation_auc'].append(self.auc_val(val_list))
+            self.loss_curve['val_accuracy'].append(self.classification_accuracy(val_list))
+            self.loss_curve['precision_at_5'].append(self.precision_at_n(n=5, train_list=train_list))
             print(f"calc evaluation AUC: {self.loss_curve['validation_auc'][self.current_epoch]:.3f}")
             print(f"total train log likelihood: {self.loss_curve['training_loglike'][self.current_epoch]:.3f}")
-
             # ----  early stopping ---- #
             # Early stopping
             if self.current_epoch > self.early_stopping_lag:
@@ -149,18 +178,22 @@ class BPR:
 
     def plot_learning_curve(self):
         # ---- plotting the validation and training ---- #
-        fig, (ax1,ax2) = plt.subplots(1,2,figsize=(10, 5))
+        fig, ax = plt.subplots(2,2,figsize=(10, 5))
 
         epochs = epochs = range(1, len(self.loss_curve['training_loglike'])+ 1)
 
         #left side
-        tr_mse = ax1.plot(epochs, self.loss_curve['training_loglike'], 'g', label='Training Loss (Normalized log-likelihood)')
-        val_mse = ax1.plot(epochs, self.loss_curve['validation_loglike'], 'b', label='Validation Loss (Normalized log-likelihood)')
-        ax1.legend()
+        tr_mse = ax[0,0].plot(epochs, self.loss_curve['training_loglike'], 'b', label='Training Loss (Normalized log-likelihood)')
+        # val_mse = ax[0,0].plot(epochs, self.loss_curve['validation_loglike'], 'g', label='Validation Loss (Normalized log-likelihood)')
+        ax[0,0].legend()
         #left side
-        tr_mse = ax2.plot(epochs, self.loss_curve['validation_auc'], 'b', label='Validation AUC')
-        ax2.legend()
-
+        tr_mse = ax[0,1].plot(epochs, self.loss_curve['validation_auc'], 'g', label='Validation AUC')
+        ax[0,1].legend()
+        #bottom
+        tr_mse = ax[1, 0].plot(epochs, self.loss_curve['val_accuracy'], 'b', label='Validation accuracy')
+        ax[1, 0].legend()
+        tr_mse = ax[1, 1].plot(epochs, self.loss_curve['precision_at_5'], 'g', label='Training precision_at_5')
+        ax[1, 1].legend()
         return fig.axes
 
 def hyper_param_tuning(params, S_train, S_valid, sample_method):

hw1_adv_rec_systems/modules/data_bpr.py
@@ -12,6 +12,7 @@ class prep_data(object):
         self.users = None
         self.sample_set = (sample_users, sample_items)
         self.get_items_users()
+        self.train=self.load_sessions_file(TRAIN_BPR_PATH)
         self.train_list=None
         self.val_list=None
 
@@ -38,6 +39,7 @@ class prep_data(object):
         for col in df.columns:
             df[col] = df[col] - 1
         return df
+
     def leave_one_out(self,sess_df):
         # np.random.seed(9)
         sess_df_1 = sess_df.groupby('UserID').ItemID.apply(
@@ -98,34 +100,109 @@ class prep_data(object):
                 neg=list(np.delete(self.items,pos_index))
             session_list.append((u, pos, neg))
         return session_list
-    def get_train_val_lists(self,neg_method='uniform'):
+
+    def subset_train(self,n_users=10,n_items=10):
+        """
+        The major challange is to reindex the items and users to start from zero
+        :param n_users:
+        :param n_items:
+        :return:
+        """
+        sess_df=self.load_sessions_file(TRAIN_BPR_PATH)
+        items_sample=np.random.choice(a=sess_df['ItemID'],size=n_items,replace=False)
+        new_train=sess_df[sess_df['ItemID'].isin(items_sample)].copy()
+
+        users_sample = np.random.choice(a=new_train['UserID'], size=n_users, replace=False)
+        new_train=new_train[new_train['UserID'].isin(users_sample)].copy()
+
+        u = new_train['UserID'].unique()
+        new_user_idx=dict(zip(u,np.arange(len(u))))
+        new_train['UserID']=new_train['UserID'].map(new_user_idx)
+        self.users=np.arange(len(u))
+
+        i=new_train['ItemID'].unique()
+        new_item_idx = dict(zip(i, np.arange(len(i))))
+        new_train['ItemID']=new_train['ItemID'].map(new_item_idx)
+        self.items=np.arange(len(i))
+
+        return new_train
+
+    def split_train_test(self, sess_df,val_quant=0.2):
+
+        np.random.seed(9)
+        sess_df_val = sess_df.groupby('UserID').ItemID.apply(
+            lambda x: x.sample(n=int(val_quant*len(x)))).reset_index()[['UserID', 'ItemID']]
+        idx_to_remove = list(zip(sess_df_val['UserID'], sess_df_val['ItemID']))
+        sess_df_train= sess_df.set_index(['UserID', 'ItemID']).drop(index=idx_to_remove).reset_index()
+        sess_df_train.head()
+
+        # sanity check:
+        assert (len(sess_df_val) + len(sess_df_train) == len(sess_df))
+        print(f"training size: {len(sess_df_train) }, val size: {len(sess_df_val) } , ratio: {len(sess_df_val)/len(sess_df_train)}")
+        return sess_df_val,sess_df_train
+
+    def get_train_val_lists(self,neg_method='uniform',val_type='leave_one_out',val_quant=0.2):
         if self.sample_set!=(None,None):
             sess_df_all = self.load_sessions_file(TRAIN_BPR_PATH)
             sess_df_all=sess_df_all[sess_df_all['UserID'].isin(self.users) & sess_df_all['ItemID'].isin(self.items) ]
         else:
             sess_df_all=self.load_sessions_file(TRAIN_BPR_PATH)
-        sess_df_val, sess_df_train =self.leave_one_out(sess_df_all)
-        self.train_list=self.get_training_list(sess_df_train,neg_method=neg_method)
-        self.val_list = self.get_training_list(sess_df_val, neg_method='all_others')
+        if val_type=='leave_one_out':
+            sess_df_val, sess_df_train =self.leave_one_out(sess_df_all)
+        else:
+            sess_df_val,sess_df_train = self.split_train_test(sess_df_all,val_quant)
+        self.train_list=self.get_training_list(sess_df_train,neg_method=neg_method).copy()
+        self.val_list = self.get_training_list(sess_df_val, neg_method='all_others').copy()
         return self.train_list,self.val_list
+
+
     def save_local_train_val_list(self,pkl_path):
         # save to local file
         outfile = open(pkl_path, 'wb')
         print(f"saving lists to disk {pkl_path}")
         pickle.dump( (self.train_list,self.val_list), outfile)
         outfile.close()
+
     def load_local_train_val_list(self,pkl_path):
         print(f"loading lists from disk {pkl_path}")
         self.train_list, self.val_list = pickle.load(open(pkl_path, 'rb'))
         return self.train_list, self.val_list
 
 if __name__ == '__main__':
-    rd=prep_data(sample_users=100,sample_items=50)
-    train_list,val_list=rd.get_train_val_lists(neg_method='uniform')
-    # rd.save_local_train_val_list(pkl_path="saved_models/train_val_uniform.pkl")
-    # train_list, val_list = rd.load_local_train_val_list("saved_models/train_val.pkl")
+    # rd=prep_data(sample_users=100,sample_items=50)
+    # train_list,val_list=rd.get_train_val_lists(neg_method='uniform')
+
+    rd=prep_data()
+    #20 split
+    train_list, val_list = rd.get_train_val_lists(neg_method='uniform',val_type='normal',val_quant=0.2)
+
+    #leave one out
+    # train_list, val_list = rd.get_train_val_lists(neg_method='uniform')
 
     #short viz of the data model
+    for ses in val_list:
+        u, p, n = ses
+        print(u, p[0:5], n[0:10])
     for ses in train_list:
         u, p, n = ses
         print(u, p[0:5], n[0:10])
+
+    u1,p1,n=train_list[6039]
+    u2,p2,n=val_list[6039]
+
+    print(u1,p1)
+    print(u2,p2)
+    # #print to file
+    # with open('test_train.txt','w') as f:
+    #     for ses in train_list:
+    #         u, p, n = ses
+    #         for p_i in p:
+    #             f.write(f"{u} {p_i}\n")
+    #         print(u, p[0:5], n[0:10])
+    # with open('test_val.txt','w') as f:
+    #     for ses in val_list:
+    #         u, p, n = ses
+    #         for p_i in p:
+    #             f.write(f"{u} {p_i}\n")
+    #         print(u, p[0:5], n[0:10])
+

hw1_adv_rec_systems/modules/hyper_search.py
@@ -29,8 +29,8 @@ class hyper_search_optimizer(object):
         :param models_params: which parameters we run for all candidates
         :param candidate_params: which parameters we search in (uniform)
         """
-        self.models_params=models_params
-        self.candidate_params=candidate_params
+        self.models_params=models_params #the dictionary with 1 instance choice of all parameters
+        self.candidate_params=candidate_params #the dictionary with the search options
 
     def select_random_option(self,x):
         i = np.random.randint(0, len(x))
@@ -40,8 +40,9 @@ class hyper_search_optimizer(object):
         candidate = {}
         for param_type in self.candidate_params.keys():
             if type(self.models_params[param_type]) == dict:
+                #this case our parameter has a dictionary for with sub parameters
                 param_sub = {}
-                for sub_val in self.models_params.keys():
+                for sub_val in self.models_params[param_type].keys():
                     x = self.select_random_option(self.candidate_params[param_type])
                     param_sub[sub_val] = x
                 candidate[param_type] = param_sub
@@ -85,7 +86,7 @@ class hyper_search_optimizer(object):
         ##---- update relevant metrics ----#
             candidate['naive_error']=0.5
             candidate['max_AUC'] = max(c_model.loss_curve['validation_auc'])
-            candidate['best_epoch'] = np.argmin(c_model.loss_curve['validation_loglike'])
+            candidate['best_epoch'] = np.argmax(c_model.loss_curve['validation_auc'])
             candidate['% improvement']=candidate['max_AUC']/candidate['naive_error']-1
             candidate['elapsed']=str(datetime.timedelta(seconds=end_run-start_run))
 
@@ -108,8 +109,14 @@ if __name__=='__main__':
     from modules.bpr import *
     from modules.data_bpr import prep_data
 
-    # rd = prep_data(sample_users=100, sample_items=20)
-    rd=prep_data()
+    sampling = True
+    sample_users = 100
+    sample_items = 50
+
+    if sampling:
+        rd = prep_data(sample_users=100, sample_items=50)
+    else:
+        rd = prep_data()
     train_list, val_list = rd.get_train_val_lists(neg_method='uniform')
 
     hp=hyper_search_optimizer(models_params=BPR_PARAMS,candidate_params=BPR_CANDIDATE_PARAMS)